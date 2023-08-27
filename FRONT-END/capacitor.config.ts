6cf1ebfb0b7aa9a63eaf337b0f151169cb1a2409
MOTURI SANTHI RAJU <135103370+santhi1213@users.noreply.github.com>
2023-08-27 11:58:43 +0530

    Add files via upload

---
+++ FRONT-END/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sponsership',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
